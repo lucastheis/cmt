ec4ff1d501214161388f06380cc6c2a02d1f4a86
Lucas Theis <lucas@theis.io>
2013-03-06 16:38:19 +0100

    Added missing parallel compiler.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ sys.path.append('./code')
 
 from distutils.core import setup, Extension
 from distutils.ccompiler import CCompiler, get_default_compiler
-from tools import parallelCCompiler
+from utils import parallelCCompiler
 from numpy.distutils.intelccompiler import IntelCCompiler
 from numpy import any
 
