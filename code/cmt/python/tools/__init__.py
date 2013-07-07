9aad06822d40e606ba65508ae0973f6db0cdfe05
Lucas Theis <lucas@theis.io>
2013-07-07 02:55:38 +0200

    Added function for creating masks.

--- code/cmt/python/tools/__init__.py
+++ code/cmt/python/tools/__init__.py
@@ -6,4 +6,5 @@ from _cmt import fill_in_image
 from _cmt import fill_in_image_map
 from _cmt import extract_windows
 from _cmt import sample_spike_train
+from masks import generate_masks
 from colors import rgb2gray, rgb2ycc, ycc2rgb, YCbCr
